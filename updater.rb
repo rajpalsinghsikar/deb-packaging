caf4849d22d9c55912d51623ac5221c51fc44964
Nikhi Jamedhar <nikhilg@thoughtworks.com>
2016-01-27 17:41:31 +0530

    Nikhil/Sattar - Add the rest of the files; Generate deb

--- updater.rb
+++ updater.rb
@@ -40,32 +40,80 @@ apps = [
 
 def uri_for(app)
   "#{BASE_URL}/#{app}/latest/#{app}_en.html"
+  #"http://localhost/#{app}_en.html"
 end
 
 def download_app(app)
   `wget -nv #{uri_for(app)}`
 end
 
-def generate_deb_files(app)
+def generate_tar(app, version)
+  appWithVersion = "#{app}-#{version}"
+  Dir.mkdir(appWithVersion)
+  Dir.chdir(appWithVersion) do
+    download_app(app)
+  end
+  tar_filename = "#{app}_#{version}.orig.tar.gz"
+  `tar czf #{tar_filename} #{appWithVersion}`
+  FileUtils.rm_rf(appWithVersion)
+  tar_filename
+end 
+
+def extract_tar(filename)
+  `tar xzf #{filename}`
+end
+
+def generate_deb_files(app, version)
   puts "Generating Deb files ..."
-  FileUtils.rm_rf app
-  Dir.mkdir(app)
-  Dir.chdir(app) do
-    Dir.mkdir("#{app}-1.0")
-    Dir.chdir("#{app}-1.0") do
-      Dir.mkdir('debian')
-      Dir.chdir('debian') do
-        generate_changelog(app)
-        generate_control(app)
-        generate_compat(app)
-      end
-    end
+  Dir.mkdir('debian')
+  Dir.chdir('debian') do
+    generate_changelog(app)
+    generate_control(app)
+    generate_compat(app)
+    generate_copyright(app)
+    generate_rules(app)
+    generate_install(app)
+    generate_format(app)
   end
   puts ".. Done!"
 end
 
+def generate_copyright(app)
+  contents = <<-FILE.gsub(/^ {4}/, '')
+    GPL V3
+  FILE
+  File.write('copyright', contents)
+end
+
+def generate_rules(app)
+  contents = <<-FILE.gsub(/^ {4}/, '')
+    #!/usr/bin/make -f
+    %:
+    	dh $@
+    override_dh_usrlocal:
+  FILE
+  File.write("rules", contents)
+end
+
+def generate_format(app)
+  Dir.mkdir('source')
+  Dir.chdir('source') do
+    contents = <<-FILE.gsub(/^ {6}/, '')
+      3.0 (quilt)
+    FILE
+    File.write('format', contents)
+  end
+end
+
+def generate_install(app)
+  contents = <<-FILE.gsub(/^ {4}/, '')
+    #{app}_en.html usr/local/lib/balaswecha/html
+  FILE
+  File.write("#{app}.install", contents)
+end
+
 def generate_control(app)
-  contents = <<-FILE
+  contents = <<-FILE.gsub(/^ {4}/, '')
     Source: #{app}
     Maintainer: Balaswecha Team<balaswecha-dev-team@thoughtworks.com>
     Section: misc
@@ -77,18 +125,18 @@ def generate_control(app)
     Architecture: any
     Depends: ${shlibs:Depends}, ${misc:Depends}
     Description: #{app}
-    #{app} is an educational simulation.
+     #{app} is an educational simulation.
   FILE
   File.write('control', contents)
 end
 
 def generate_changelog(app)
-  contents = <<-FILE
+  contents = <<-FILE.gsub(/^ {4}/, '')
     #{app} (1.0-1) UNRELEASED; urgency=low
 
-     * Initial release.
+      * Initial release. (Closes: #XXXXX)
 
-    -- Balaswecha Team <balaswecha-dev-team@thoughtworks.com>  #{Time.now.strftime '%a, %-d %b %Y %H:%M:%S %z'}
+     -- Balaswecha Team <balaswecha-dev-team@thoughtworks.com>  #{Time.now.strftime '%a, %-d %b %Y %H:%M:%S %z'}
   FILE
   File.write('changelog', contents)
 end
@@ -97,15 +145,24 @@ def generate_compat(app)
   File.write('compat', "9\n")
 end
 
-def generate_deb(app)
-  Dir.mkdir('dist')
-  Dir.chdir('dist') do
-    download_app(app)
-    generate_deb_files(app)
-  end
+def generate_deb
+  `debuild -us -uc`
 end
 
-#apps = apps.take(1)
-apps.each do |app|
-  generate_deb(app)
+FileUtils.rm_rf 'dist'
+Dir.mkdir('dist')
+Dir.chdir('dist') do
+  #apps = apps.take(1)
+  apps.each do |app|
+    Dir.mkdir(app)
+    Dir.chdir(app) do
+      version = "1.0"
+      tar_filename = generate_tar(app, version)
+      extract_tar(tar_filename)
+      Dir.chdir("#{app}-#{version}") do
+        generate_deb_files(app, version)
+        generate_deb
+      end
+    end
+  end
 end
